e2011d01fa453c2875a1f71fc0ad1fdedad4eb2b
Arifin79 <arifin.nurhidayah1933@gmail.com>
2024-09-30 11:45:30 +0700

    dashboard

---
+++ resources/views/dashboard/layout/header.blade.php
@@ -0,0 +1,56 @@
+<!-- Spinner Start -->
+<div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+    <div class="spinner-border position-relative text-primary" style="width: 6rem; height: 6rem;" role="status"></div>
+    <i class="fa fa-laptop-code fa-2x text-primary position-absolute top-50 start-50 translate-middle"></i>
+</div>
+<!-- Spinner End -->
+
+<!-- Brand & Contact Start -->
+<div class="container-fluid py-4 px-5 wow fadeIn" data-wow-delay="0.1s">
+    <div class="row align-items-center top-bar">
+        <div class="col-lg-4 col-md-12 text-center text-lg-start">
+            <a href="" class="navbar-brand m-0 p-0">
+                <h1 class="fw-bold text-primary m-0"><i class="fa fa-laptop-code me-3"></i>DGcom</h1>
+                <!-- <img src="img/logo.png" alt="Logo"> -->
+            </a>
+        </div>
+        <div class="col-lg-8 col-md-7 d-none d-lg-block">
+            <div class="row">
+                <div class="col-4">
+                    <div class="d-flex align-items-center justify-content-end">
+                        <div class="flex-shrink-0 btn-lg-square border rounded-circle">
+                            <i class="far fa-clock text-primary"></i>
+                        </div>
+                        <div class="ps-3">
+                            <p class="mb-2">Opening Hour</p>
+                            <h6 class="mb-0">Mon - Fri, 8:00 - 9:00</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="d-flex align-items-center justify-content-end">
+                        <div class="flex-shrink-0 btn-lg-square border rounded-circle">
+                            <i class="fa fa-phone text-primary"></i>
+                        </div>
+                        <div class="ps-3">
+                            <p class="mb-2">Call Us</p>
+                            <h6 class="mb-0">+012 345 6789</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="d-flex align-items-center justify-content-end">
+                        <div class="flex-shrink-0 btn-lg-square border rounded-circle">
+                            <i class="far fa-envelope text-primary"></i>
+                        </div>
+                        <div class="ps-3">
+                            <p class="mb-2">Email Us</p>
+                            <h6 class="mb-0">info@example.com</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Brand & Contact End -->
