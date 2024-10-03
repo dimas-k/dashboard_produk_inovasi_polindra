f3ce415f0a53b7eaeb2b55f27ebc4f5b02c5d9b0
Arifin79 <arifin.nurhidayah1933@gmail.com>
2024-10-03 15:25:57 +0700

    adjustmant dashboard

--- resources/views/dashboard/contact/content.blade.php
+++ resources/views/dashboard/contact/content.blade.php
@@ -20,7 +20,7 @@
                 <h6 class="section-title bg-white text-center text-primary px-3">Contact Us</h6>
                 <h1 class="display-6 mb-4">If You Have Any Query, Please Feel Free Contact Us</h1>
             </div>
-            <div class="row g-0 justify-content-center">
+            <div class="text-center row g-0 justify-content-center">
                 <div class="col-lg-8 wow fadeInUp" data-wow-delay="0.5s">
                     <p class=" mb-3">Politeknik Negeri Indramayu</p>
                     <p class=" mb-3">Jl. Lohbener Lama No.08, Legok, Kec. Lohbener, Kabupaten Indramayu</p>
@@ -33,8 +33,9 @@
 <!-- Contact End -->
 
 <!-- Google Map Start -->
-<div class="container-xxl pt-5 px-0 wow fadeIn" data-wow-delay="0.1s">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9018.143827847049!2d108.27225476370512!3d-6.412076912597838!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6eb87d1fcaf97d%3A0x4fc15b3c8407ada4!2sPoliteknik%20Negeri%20Indramayu!5e0!3m2!1sid!2sid!4v1727839931532!5m2!1sid!2sid"
-            width="450" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-</div>
+<div class="container-xxl pt-3 px-0 wow fadeIn" data-wow-delay="0.1s">
+        <iframe class="w-100 mb-n2" style="height: 450px;"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6473.427671449338!2d108.27999187518209!3d-6.413333251058172!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6eb87d1fcaf97d%3A0x4fc15b3c8407ada4!2sPoliteknik%20Negeri%20Indramayu!5e0!3m2!1sid!2sid!4v1727838103756!5m2!1sid!2sid"
+            frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+        </div>
 <!-- Google Map End -->
