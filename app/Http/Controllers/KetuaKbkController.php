60663d1dba2b88010a0ae65857cb0e5d1eb06be4
dimas-k <dimasarya81821@gmail.com>
2024-10-21 10:59:29 +0700

    fixing jquery di ketua sama admin

--- app/Http/Controllers/KetuaKbkController.php
+++ app/Http/Controllers/KetuaKbkController.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 
 
 class KetuaKbkController extends Controller
